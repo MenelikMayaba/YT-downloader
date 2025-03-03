a0f3ac4681463bee6540b0827839fc49275b2461
memulimjhb024 <memulimjhb024@student.wethinkcode.co.za>
2025-03-03 13:03:36 +0200

    downlaoder

--- downloader.py
+++ downloader.py
@@ -1,1 +1,12 @@
+from pytube import YouTube
 
+def main():
+    url = input("please paste the url of the video: ")
+
+    yt = YouTube(url)
+
+    yt.streams.get_highest_resolution().download
+
+
+if __name__ == "__main__":
+    main()
