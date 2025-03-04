ee670cc0fb49d9a89db84dfd28da1a2126e8947c
memulimjhb024 <memulimjhb024@student.wethinkcode.co.za>
2025-03-04 04:35:25 +0200

    main

--- downloader.py
+++ downloader.py
@@ -11,7 +11,7 @@ def main():
     continues until the user inputs 'exit' to quit. Handles exceptions and
     provides error messages for invalid inputs or download issues.
     """
-    options = ["audio", "resolution", "HD", "LD"]
+    options = ["audio", "resolution", "hd", "hd"]
 
     while True:
         url = input("Please paste the URL of the video or 'exit' to quit: ").strip()
@@ -20,30 +20,37 @@ def main():
             print("Goodbye")
             break
 
-        definition = input(f"What definition would you like?\nPlease choose from {options}: ").strip()
+        definition = input(f"What definition would you like?\nPlease choose from 1.{options[0]}\n 2.{options[1]}\n 3.{options[2]}\n 4.{options[3]}: ").strip()
 
         try:
             yt = YouTube(url)
 
-            if definition == "audio":
-                yt.streams.get_audio_only().download()
+            if definition == "audio" or definition == "1":
+                print("Downloading...")
+                stream = yt.streams.get_audio_only()
+                stream.download()
                 print("Audio downloaded successfully.")
 
-            elif definition == "resolution":
-                res = input("Enter resolution (e.g., 720p, 480p, etc.): ").strip()
+            elif definition == "resolution" or definition == "2":
+                res = input("Enter resolution (2160p, 1440p, 1080p, 720p, 480p, etc.): ").strip()
                 stream = yt.streams.filter(res=res).first()
                 if stream:
+                    print("Downloading...")
                     stream.download()
                     print(f"Video downloaded at {res} resolution.")
                 else:
                     print("Resolution not available.")
 
-            elif definition == "HD":
-                yt.streams.get_highest_resolution().download()
+            elif definition == "HD" or definition == "3":
+                print("Downloading...")
+                stream = yt.streams.get_highest_resolution()
+                stream.download()
                 print("Highest resolution downloaded.")
 
-            elif definition == "LD":
-                yt.streams.get_lowest_resolution().download()
+            elif definition == "LD" or definition == "4":
+                print("Downloading...")
+                stream = yt.streams.get_lowest_resolution()
+                stream.download()
                 print("Lowest resolution downloaded.")
 
             else:
