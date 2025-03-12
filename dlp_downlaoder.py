0406d43e8df327096d61ce2bd8ac23b426f22397
memulimjhb024 <memulimjhb024@student.wethinkcode.co.za>
2025-03-12 09:41:17 +0200

    functionality to view download history

--- dlp_downlaoder.py
+++ dlp_downlaoder.py
@@ -7,10 +7,29 @@ def download(url):
 
 def main():
     url = input("enter the link of the video you wnat to downlaod: ")
+    history = []
+    if "https" not in url:
+        raise ValueError("please insert a correct url link")
+    
+    
+    try:
+        print("downloading video...")
+        download(url)
+        print("download complete")
+        print("url saved to history")
+        history.append(url)
 
-    print("downloading video...")
-    download(url)
-    print("download complete")
+            
+            
+        url_history = input("do you want to see your download history Y/n? ").lower()
+
+        if url_history == "y":
+            print(history)
+        elif url_history == 'n':
+            pass
+
+    except ValueError:
+        print("Something went wrong, please try again")
 
 if __name__ == "__main__":
     main()
