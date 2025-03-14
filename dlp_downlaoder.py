c94625fd2cfd34399b9380d081f18092425fb25e
memulimjhb024 <memulimjhb024@student.wethinkcode.co.za>
2025-03-14 09:49:20 +0200

    refining code

--- dlp_downlaoder.py
+++ dlp_downlaoder.py
@@ -4,32 +4,44 @@ def download(url):
 
     with YoutubeDL() as ytd:
         ytd.download(url)
-
+        info = ytd.extract_info(url)
+        print(f"'{info['title']}' successfully downloaded")
+    
 def main():
-    url = input("enter the link of the video you wnat to downlaod: ")
-    history = []
-    if "https" not in url:
-        raise ValueError("please insert a correct url link")
+    while True:
+        try:
+
+            url = input("enter the link of the video you wnat to downlaod: ")
+            history = []
+            if "https" not in url:
+                raise ValueError("please insert a correct url link")
     
     
-    try:
-        print("downloading video...")
-        download(url)
-        print("download complete")
-        print("url saved to history")
-        history.append(url)
+        
+            print("downloading video...")
+            download(url)
+            print("url saved to history") 
+            history.append(url)
 
+                
+                
+            url_history = input("Show download history or exit? (Y/n/exit): ").lower()
+
+            if url_history == "y":
+                print(history)
+                continue
+            elif url_history == 'n':
+                continue
+            elif url_history == "exit":
+                print("exiting, goodbye..." )
+                break
             
+                
+                
+
+        except ValueError:
+            print("Something went wrong, please try again")
             
-        url_history = input("do you want to see your download history Y/n? ").lower()
-
-        if url_history == "y":
-            print(history)
-        elif url_history == 'n':
-            pass
-
-    except ValueError:
-        print("Something went wrong, please try again")
 
 if __name__ == "__main__":
     main()
